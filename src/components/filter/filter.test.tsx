11e6db627de983f8a39f0ee1e4a5e4de86ed27dd
dan_pocock <dan.pocock@vistair.com>
2022-11-15 16:43:47 +0000

    added comments for filter tests

--- src/components/filter/filter.test.tsx
+++ src/components/filter/filter.test.tsx
@@ -1,28 +1,41 @@
-import React from 'react';
-import { screen } from '@testing-library/react';
-import { render } from '../../test-utils';
-import Filter from './filter';
+import React from "react";
+import { screen } from "@testing-library/react";
+import { render } from "../../test-utils";
+import Filter from "./filter";
 
 const resetQuestions = () => {};
 
-const categories = ['one','two','three'];
+const categories = ["one", "two", "three"];
 const selectedCats: string[] = [];
 const noQuestionsLeftCats: string[] = [];
 const setSelectedCats = jest.fn();
 const onCloseModal = jest.fn();
 
-beforeEach(() => render(<Filter 
-    categories={categories}
-    selectedCats={selectedCats}
-    setSelectedCats={setSelectedCats}
-    onCloseModal={onCloseModal}
-    noQuestionsLeftCats={noQuestionsLeftCats} />));
+beforeEach(() =>
+  render(
+    <Filter
+      categories={categories}
+      selectedCats={selectedCats}
+      setSelectedCats={setSelectedCats}
+      onCloseModal={onCloseModal}
+      noQuestionsLeftCats={noQuestionsLeftCats}
+    />
+  )
+);
 
-describe('renders components: ', () => {
-  test('self', () => {
-    const currentElement = screen.getByTestId('FilterButton');
+describe("renders components: ", () => {
+  test("self", () => {
+    const currentElement = screen.getByTestId("FilterButton");
     expect(currentElement).toBeInTheDocument();
   });
+
+  // items to test
+  // click modal opens
+  // length of checkboxes === cats.length
+  // click close closes modal
+  // uncheck item submits as it should
+  // unchecked all stops close
+  // select all sbumits all
 
   // test('child elements ', () => {
   //   const menuButton = screen.getByTestId('menuButton');
