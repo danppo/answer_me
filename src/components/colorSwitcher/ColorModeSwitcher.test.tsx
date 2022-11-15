6a8cfcf7bb09830c3a03956d0faef5db11db9bc6
dan_pocock <dan.pocock@vistair.com>
2022-11-15 10:07:48 +0000

    fixed colorswitcher function in test

--- src/components/colorSwitcher/ColorModeSwitcher.test.tsx
+++ src/components/colorSwitcher/ColorModeSwitcher.test.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
-import { screen } from '@testing-library/react';
-import { render } from '../../test-utils';
-import ColorModeSwitcher from './ColorModeSwitcher';
+import React from "react";
+import { screen, fireEvent } from "@testing-library/react";
+import { render } from "../../test-utils";
+import ColorModeSwitcher from "./ColorModeSwitcher";
 
-describe('renders components:', () => {
-  test('self', () => {
+describe("renders components:", () => {
+  test("self", () => {
     render(<ColorModeSwitcher />);
-    const currentElement = screen.getByTestId('colorSwitcher');
+    const currentElement = screen.getByTestId("colorSwitcher");
     expect(currentElement).toBeInTheDocument();
   });
+  test("button switch color mode", () => {
+    const textInDarkMode = "Switch to light mode";
+    const textInLightMode = "Switch to dark mode";
+    render(<ColorModeSwitcher />);
+    const button = screen.getByTestId("switcherButton");
+    expect(button).toHaveAttribute("aria-label", textInLightMode);
+    fireEvent.click(button);
 
+    expect(button).toHaveAttribute("aria-label", textInDarkMode);
+  });
 });
